9fac9a4add89e0e88e9afafd1c122d761a33075c
Abishek <151960865+Abishek00ujj@users.noreply.github.com>
2024-12-03 22:57:38 +0530

    navbar

--- Client/online-auction/src/components/Navbar.jsx
+++ Client/online-auction/src/components/Navbar.jsx
@@ -2,18 +2,23 @@ import React, { useState } from 'react'
 import {Menu,EllipsisVertical,ArrowLeft} from 'lucide-react'
 import Logo from '../assets/online-auction logo.webp'
 const Navbar = ({name}) => {
-    const [scroll,setScroll]=useState(true);
+    const [scroll,setScroll]=useState(false);
+    const [change,setChange]=useState(true);
     const handleNavbar=()=>{
-        setScroll(true);
+      setScroll(true);
+      setChange(true);
         setTimeout(() => {
             setScroll(false);
-        },350);
+        },450);
+        setTimeout(() => {
+          setChange(false);
+      },600);
     };
   return (
     <div className='w-screen h-[70px] bg-[#f8f8f8] border border-black flex'>
             <div className='flex justify-start items-center space-x-4'>
             <div className={scroll?(`h-8 flex justify-start items-center p-1 animate-spin`):(`h-8 flex justify-start items-center p-1`)} onClick={handleNavbar}>
-                {!scroll?<Menu size={40}/>:<ArrowLeft size={40}/>}
+                {change?<Menu size={40}/>:<ArrowLeft size={40}/>}
                 </div>
                 <img src={Logo} alt="" className='h-[34px] w-[200px]'/>
             </div>
