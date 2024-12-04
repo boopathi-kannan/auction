04ff1ed930507c8c7fe463aec6c76d0cd57a27ba
Abishek <151960865+Abishek00ujj@users.noreply.github.com>
2024-12-04 22:39:40 +0530

    x

--- Client/online-auction/src/pages/Home.jsx
+++ Client/online-auction/src/pages/Home.jsx
@@ -4,18 +4,27 @@ import UpcomingCard from '../components/UpcomingCard'
 const Home = () => {
   return (
     <>
-    <div className='bg-amber-400 flex max-sm:flex-col-reverse max-md:flex-col-reverse justify-between items-center w-full'>
+    <div className='bg-amber-400 flex max-sm:flex-col-reverse max-md:flex-col-reverse justify-between items-center w-full h-full'>
       <div className='p-3 text-center w-1/2 self-center'>
         <h1 className='text-5xl m-5'>The best place</h1>
         <h1 className='font-semibold text-5xl m-5'>to buy and sell!</h1>
         <button className='bg-black text-white p-3 font-medium m-5'>Register</button>
       </div>
-    <div className='w-1/2 flex justify-center items-center'>
+    <div className='w-full flex justify-center items-center h-full'>
       <img src='https://res.cloudinary.com/dve8r06ul/image/upload/v1733240064/auctionhome1_tcipqj.avif' className='h-full'/>
     </div>
     </div>
+  <div className='flex overflow-x-auto'>
+  {[...Array(10)].map((_, index) => (
+    <FinishedCard key={index} />
+  ))}
+ </div>
+ <div className='flex overflow-x-auto'>
+  {[...Array(10)].map((_, index) => (
+    <UpcomingCard key={index} />
+  ))}
+ </div>
     </>
   )
 }
-
 export default Home;
