0229a2aba0f0c2ac164a3b9aba2789bd0811938e
Abishek <151960865+Abishek00ujj@users.noreply.github.com>
2024-12-06 23:51:40 +0530

    mm

--- Client/online-auction/src/components/Navbar.jsx
+++ Client/online-auction/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
-import {Menu,EllipsisVertical,ArrowLeft} from 'lucide-react'
+import {Menu,EllipsisVertical,ArrowLeft,Phone} from 'lucide-react'
 import Logo from '../assets/online-auction logo.webp'
 const Navbar = ({name}) => {
     const [scroll,setScroll]=useState(false);
     const [change,setChange]=useState(true);
+    const [downbar,setDownbar]=useState(false);
     const handleNavbar=()=>{
       setScroll(true);
       setChange(true);
@@ -14,8 +15,12 @@ const Navbar = ({name}) => {
           setChange(false);
       },600);
     };
+    const handleDownbar=()=>{
+       setDownbar(!downbar);
+    }
   return (
-    <div className='w-screen h-[70px] bg-[#f8f8f8] border flex'>
+    <>
+    <div className='w-screen h-[70px] bg-[#f8f8f8] border flex relative'>
             <div className='flex justify-start items-center space-x-4'>
             <div className={scroll?(`h-8 flex justify-start items-center p-1 animate-spin`):(`h-8 flex justify-start items-center p-1`)} onClick={handleNavbar}>
                 {change?<Menu size={40}/>:<ArrowLeft size={40}/>}
@@ -23,8 +28,41 @@ const Navbar = ({name}) => {
                 <img src={Logo} alt="" className='h-[34px] w-[200px]'/>
             </div>
             <p>{name}</p>
-            <div className='w-full flex justify-end items-center p-2'><EllipsisVertical size={35}/></div>
+            <div className='w-full flex justify-end items-center p-2'><EllipsisVertical size={35} onClick={handleDownbar}/></div>
     </div>
+      {
+        downbar&&(
+        <div className='w-screen flex justify-end fixed'>
+        <div className='w-[80%] h-auto bg-[#f8f8f8] p-8'>
+             <div className='w-full h-full flex flex-col'>
+              <div className='w-full flex'>
+                <div className='w-[20%] h-[50%] flex items-center justify-center'>
+                    <Phone fill='orange'size={40}/>
+                </div>
+                <div className='w-full text-2xl flex flex-col'>
+                    <div className='w-full flex justify-start items-center'>
+                        Freephone
+                    </div>
+                    <div className='w-full flex justify-start items-center'>
+                       800-2345-6789
+                    </div>
+                </div>
+                </div>
+                <div className='w-full h-[80%] flex flex-col'>
+                  <div>
+                    Opening Hours 8am-8pm PST M-Th;6am-3pm PST Fri
+                  </div>
+                  <div className='flex flex-col w-full gap-3'>
+                      <button className='bg-black text-white pl-5 pr-5 pt-3 pb-3 rounded-md'>Login</button>
+                      <button className='bg-yellow-400 text-white pl-5 pr-5 pt-3 pb-3 rounded-md'>Register</button>
+                  </div>
+                </div>
+                </div>
+        </div>
+        </div>
+        )
+      }
+   </>
   );
 }
 
--- Client/online-auction/src/pages/Home.jsx
+++ Client/online-auction/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import FinishedCard from '../components/FinishedCard'
 import UpcomingCard from '../components/UpcomingCard'
+import {ChevronRight} from 'lucide-react'
 const Home = () => {
   return (
     <>
@@ -8,17 +9,27 @@ const Home = () => {
       <div className='p-3 text-center w-1/2 self-center'>
         <h1 className='text-5xl m-5'>The best place</h1>
         <h1 className='font-semibold text-5xl m-5'>to buy and sell!</h1>
-        <button className='bg-black text-white p-3 font-medium m-5'>Register</button>
+       <div className='w-full flex justify-center'> <button className='bg-black text-white pl-5 pr-5 pt-3 pb-3 rounded-md font-bold m-5 flex gap-2'>Register <span className='text-orange-400 font-bold'><ChevronRight /></span> </button></div>
       </div>
     <div className='w-full flex justify-center items-center h-full'>
       <img src='https://res.cloudinary.com/dve8r06ul/image/upload/v1733240064/auctionhome1_tcipqj.avif' className='h-full'/>
     </div>
+    </div>
+    <div className='w-full flex justify-start mt-10 mb-10 pl-10'>
+      <p className='font-light text-4xl text-gray-400'>
+        <span className='font-bold text-black'>Finished </span> Auctions
+      </p>
     </div>
   <div className='flex overflow-x-auto'>
   {[...Array(10)].map((_, index) => (
     <FinishedCard key={index} />
   ))}
  </div>
+ <div className='w-full flex justify-start mt-10 mb-10 pl-10'>
+      <p className='font-light text-4xl text-gray-400'>
+        <span className='font-bold text-black'>Current </span> Auctions
+      </p>
+    </div>
  <div className='flex overflow-x-auto'>
   {[...Array(10)].map((_, index) => (
     <UpcomingCard key={index} />
